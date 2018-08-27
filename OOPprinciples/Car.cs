2fbc2f83b4a04086d17b8da6a7678c6385a4c67d
John Paul <greerjohnpaul@gmail.com>
2018-08-27 08:47:07 -0500

    added abstraction

--- OOPprinciples/Car.cs
+++ OOPprinciples/Car.cs
@@ -3,6 +3,30 @@ namespace OOPprinciples
 {
     public class Car
     {
-        
+        //this is an example of abstraction
+
+        public int numOfTires;
+        public string Model;
+
+        public void setNumberOfTires(int tires)
+        {
+            numOfTires = tires;
+        }
+        public int GetNumberOfTires()
+        {
+            return numOfTires;
+        }
+        public void SetModel(string model)
+        {
+            if (String.IsNullOrWhiteSpace(model))
+            {
+                Model = "F-250";
+            }
+            Model = model;
+        }
+        public string GetModel()
+        {
+            return Model;
+        }
     }
 }
